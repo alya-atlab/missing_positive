2e22e0305c140ddb4b3dda2d1aec4e6d569ea7cd
Alya Atlab <133522435+alya-atlab@users.noreply.github.com>
2024-11-04 14:33:24 +0200

    Add some comments

--- missing_positive.py
+++ missing_positive.py
@@ -1,9 +1,10 @@
 def first_missing_positive(numbers):
-    maxnum = max(numbers)
-    for i in range (1, maxnum):
-        if( i not in numbers):           
-            return i
-    return maxnum + 1
+    maxnum = max(numbers) #knowing the max number to loop from 1 to it
+    for i in range (1, maxnum): 
+        if( i not in numbers):  # if the number is not in the list so this is the first number missed    
+            return i 
+    return maxnum + 1 #if the loop finish and there is no missed number so the first missed number is the number greater then the max
     
     
     
+print(first_missing_positive([-1, -2, -3]))
